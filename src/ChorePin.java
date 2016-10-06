a8e9a2d3311b36e4206dff7d252cc36260fc06f4
Edison <edisonsgrauman@gmail.com>
2016-10-06 15:48:59 -0700

    refactored ChorePin

--- src/ChorePin.java
+++ src/ChorePin.java
@@ -1,10 +1,18 @@
-import java.awt.*;
-import java.awt.geom.AffineTransform;
+//import java.awt.*;
+//import java.awt.geom.AffineTransform;
 import static java.lang.Math.*;
 
+import javafx.collections.ObservableList;
+import javafx.geometry.Point2D;
+import javafx.scene.shape.ArcType;
+import javafx.scene.shape.Polygon;
+import javafx.scene.transform.Affine;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+
 public class ChorePin {
-    private Shape curShape;
-    private Shape origShape;
+    private Polygon origShape;
     private double theta = 0;
     private double angularVel = 0;
     private double angularAcc = 0;
@@ -12,9 +20,10 @@ public class ChorePin {
     private boolean isHit = false;
 
     public ChorePin() {
-        int[] x = new int[] { 0, 1, 1, 1, 2, 0, -2, -1, -1, -1, 0 };
-        int[] y = new int[] { 0, 0, 2, 4, 4, 8,  4,  4,  2,  0, 0 };
-        origShape = new Polygon(x, y, 11);
+//        int[] x = new int[] { 0, 1, 1, 1, 2, 0, -2, -1, -1, -1, 0 };
+//        int[] y = new int[] { 0, 0, 2, 4, 4, 8,  4,  4,  2,  0, 0 };
+        origShape = new Polygon(0,0,1,0,1,2,1,4,2,4,0,8,-2,4,-1,4,-1,2,-1,0,0,0);
+
         hit(PI/20);
     }
 
@@ -49,33 +58,45 @@ public class ChorePin {
         return 100*abs(theta%(2*PI)) + 150*abs(angularVel)*abs(angularVel);
     }
 
-    public Point getXY() {
-        return new Point((int) (curShape.getBounds().x  + .5*curShape.getBounds().width), curShape.getBounds().y + curShape.getBounds().height);
+    public Point2D getColorSamplePoint() {
+        return new Point2D(800, 400);
     }
 
-    public Point getColorSamplePoint() {
-        return new Point(800, 400);
-    }
-
-    public void paint(Graphics g) {
-        Graphics2D g2 = (Graphics2D) g;
+    public void paint(GraphicsContext g2) {
 
         updatePhysics();
-        AffineTransform trans = new AffineTransform();
-        trans.concatenate(AffineTransform.getTranslateInstance(800, 55));
-        trans.concatenate(AffineTransform.getScaleInstance(35, 35));
-        trans.concatenate(AffineTransform.getRotateInstance(theta));
-        curShape = trans.createTransformedShape(origShape);
+        Affine trans = new Affine();
+        trans.append(Affine.translate(800, 55));
+        trans.append(Affine.scale(35, 35));
+        trans.append(Affine.rotate(theta,0,0));
+        origShape.getTransforms().add(trans);
 
-        g2.setColor(Color.GRAY);
-        g2.fill(curShape);
-        g2.setColor(Color.BLACK);
-        g2.draw(curShape);
+
+
+        g2.setFill(Color.BLACK);
+
+        ObservableList<Double> l = origShape.getPoints(); //fixme
+
+        double[] xpoints = new double[11];
+        double[] ypoints = new double[11];
+
+        for(int i = 0, xI = 0, yI = 0; i < l.size(); i++) {
+            if(i % 2 == 0) {
+                xpoints[xI] = l.get(i);
+                xI++;
+            }
+            else {
+                ypoints[yI] = l.get(i);
+                yI++;
+            }
+        }
+
+        g2.fillPolygon(xpoints, ypoints, 11);
 
         int r = 20;
-        g2.setColor(Color.LIGHT_GRAY);
-        g2.fillArc(800 - r/2, 55 - r/2, r, r, 0, 360);
-        g2.setColor(Color.BLACK);
-        g2.drawArc(800 - r/2, 55 - r/2, r, r, 0, 360);
+        g2.setFill(Color.LIGHTGRAY);
+        g2.fillArc(800 - r/2, 55 - r/2, r, r, 0, 360, ArcType.ROUND);
+        g2.setStroke(Color.BLACK);
+        g2.strokeArc(800 - r/2, 55 - r/2, r, r, 0, 360, ArcType.ROUND);
     }
 }
