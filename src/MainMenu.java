085ebbc3c3fe4849c5e88745360d1f7688425404
Edison <edisonsgrauman@gmail.com>
2016-10-12 12:35:19 -0700

    messed with main menu alignment and label for no entity

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -3,23 +3,41 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 
 /**
  * Created by Edison on 10/10/16.
  */
 
-public class MainMenu extends FlowPane {
+public class MainMenu extends GridPane {
 
     private ChoreWheelRun run;
 
     private Button spinButton, configButton;
+    private Label warningLabel;
 
 
     MainMenu(ChoreWheelRun choreWheelRun) {
         run = choreWheelRun;
 
+        HBox hbox1 = new HBox();
+
+
+
         spinButton = new Button("Spin!");
         configButton = new Button("Config");
+        warningLabel = new Label("");
+
+        HBox.setHgrow(spinButton, Priority.NEVER);
+        HBox.setHgrow(configButton, Priority.NEVER);
+        spinButton.setMaxWidth(Double.MAX_VALUE);
+        configButton.setMaxWidth(Double.MAX_VALUE);
+
+        hbox1.getChildren().addAll(spinButton, configButton);
+
+        hbox1.setSpacing(10);
         spinButton.setOnAction(e -> ButtonClicked(e));
         configButton.setOnAction(e -> ButtonClicked(e));
 
@@ -27,8 +45,12 @@ public class MainMenu extends FlowPane {
         setHgap(10);
         setAlignment(Pos.CENTER);
 
-        getChildren().addAll(spinButton, configButton);
-//        setAutoScale(AutoScale.FIT);
+        warningLabel.setMaxWidth(100);
+        warningLabel.setWrapText(true);
+
+        add(hbox1,0,0);
+        add(warningLabel,0,2);
+
 
     }
 
@@ -40,9 +62,10 @@ public class MainMenu extends FlowPane {
                 run.wheel.setChores();
 
                 if(run.wheel.getNames().isEmpty() || run.wheel.getChores().isEmpty()) {
-                    System.out.println("NO ENTITIES");
+                    warningLabel.setText("Please add at least one name and one chore");
                 }
                 else {
+                    warningLabel.setText("");
                     run.wheel.populateChores();
                     run.theStage.setScene(run.spinScene);
                 }
